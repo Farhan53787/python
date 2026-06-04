1c6fedb03ec7768d25511727a0df899d66aaa8ac
Farhan53787 <codgerfarhan@gmail.com>
2026-06-04 21:58:16 +0600

    Add files via upload

---
+++ fruits.py
@@ -0,0 +1,26 @@
+fruits = ["mango", "lichi", "orange", "apple", "banana"]
+
+print("I love " + (fruits[0]) + " the most.")
+
+print(f"I love {len(fruits)} fruits the most.")
+
+fruits.append("grapes")
+print(fruits)
+
+fruits.remove("apple")
+print(fruits)
+
+fruits.sort()
+print(fruits)
+
+fruits.reverse()
+print(fruits)
+
+fruits.insert(2, "kiwi")
+print(fruits)
+
+fruits.pop()
+print(fruits)
+
+fruits.clear()
+print(fruits)
