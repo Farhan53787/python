3a082bdcd8049eb610321670cbf01b97ca950cec
Farhan53787 <codgerfarhan@gmail.com>
2026-06-18 17:58:45 +0600

    Add files via upload

---
+++ Calculator.py
@@ -0,0 +1,21 @@
+Oper = input("Operation you want to perform:")
+
+if Oper == "addition":
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(num1 + num2)
+
+elif Oper == "subtraction":
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(num1 - num2)
+
+elif Oper == "multiplication":
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(num1 * num2)
+
+elif Oper == "division":
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print(num1 / num2)
