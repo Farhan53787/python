b3d316319e0e6ae7102c88dc298184b88de8cea6
Farhan53787 <codgerfarhan@gmail.com>
2026-06-24 21:29:13 +0600

    Add files via upload

---
+++ Tuple.py
@@ -0,0 +1,22 @@
+#Tuples use Parentheses
+tuple = ()
+print(tuple)
+
+tuple = ("python", "venomous", "6")
+print (tuple)
+
+tuple = ("Ronaldo", [78,93,0], ("Portugal","goat", 7))
+print(tuple)
+
+tuple = (4, "t", "o", "ys")
+print(tuple[1])
+print(tuple[3])
+
+tuple = ("key", [4,8,1,2], (4,9,8))
+print(tuple[2][-1])
+print(tuple[1][0])
+
+print("sliced:" [2:3])
+
+for letter in (tuple):
+    print("Go on", letter)
