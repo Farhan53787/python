6679b49a85fa4a015f5baa2b8938198979b38450
Farhan53787 <codgerfarhan@gmail.com>
2026-06-25 21:31:08 +0600

    Add files via upload

---
+++ Patterns.py
@@ -0,0 +1,10 @@
+print ("Patterns")
+for i in range(1,10):
+   for u in range(i):
+    print ("$", end=" ")
+   print("/n")
+
+for i in range(10,1,-1):
+   for j in range(i):
+    print ("$", end=" ")
+   print ("/n")
